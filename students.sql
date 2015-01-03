26d87e526597190c2ff2cafb977a3454710cbf9d
MindiMakridi <thehabibass@gmail.com>
2015-01-03 19:01:11 +0300

    sql

--- students.sql
+++ students.sql
@@ -27,12 +27,12 @@ CREATE TABLE `students` (
   `name` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `sname` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `birthdate` int(11) DEFAULT NULL,
+  `birthdate` year(4) DEFAULT NULL,
   `points` int(11) DEFAULT NULL,
-  `sex` varchar(4) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `groupindex` int(11) DEFAULT NULL,
+  `sex` enum('М','Ж') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `groupindex` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,'Василий','Серебряков','theguardian@hotmail.com',1995,189,'М',8723),(2,'Никита','Артамонов','Nikita99@yandex.ru',1995,150,'М',1473),(3,'Сергей','Швецов','Semen215@yandex.ru',1993,18,'М',8344),(4,'Григорий','Симоненко','thybb@lel.com',2001,499,'М',9977),(5,'Алина','Назарбаева','Alinafemnazi@gore.com',1999,40,'Ж',8300),(6,'Степан','Брандибак','StepanDestroyer@mail.ru',1987,315,'М',8500),(7,'Николай','Расторгуев','Rastorguev@hotmail.com',1955,199,'М',8800),(8,'Волчара\r\n','Абдлгн','cornerstone@gmail.com',1997,280,'Ж',9300),(9,'Винни','Керчесов','Sick@sa.ru',1901,112,'М',1123),(10,'Николай','Валуев','Valued23@mail.ru',1971,400,'М',1333),(11,'Себастьян','Куросава','Dima666@mail.ru',1993,290,'М',9342),(12,'Николай','Ключников','bane@mail.ru',1995,186,'М',89333),(13,'Дмитрий','Черепаха','fixit@mail.ru',1998,99,'М',8888),(14,'Рихард','Гиот','giot@gmail.com',1900,448,'М',6666);
+INSERT INTO `students` VALUES (1,'Василий','Серебряков','theguardian@hotmail.com',1995,189,'М','8723'),(2,'Никита','Артамонов','Nikita99@yandex.ru',1995,150,'М','1473'),(3,'Сергей','Швецов','Semen215@yandex.ru',1993,18,'М','8344'),(4,'Григорий','Симоненко','thybb@lel.com',2001,499,'М','9977'),(5,'Алина','Назарбаева','Alinafemnazi@gore.com',1999,40,'Ж','8300'),(6,'Степан','Брандибак','StepanDestroyer@mail.ru',1987,315,'М','8500'),(7,'Николай','Расторгуев','Rastorguev@hotmail.com',1955,199,'М','8800'),(8,'Волчара\r\n','Абдлгн','cornerstone@gmail.com',1997,280,'Ж','9300'),(9,'Винни','Керчесов','Sick@sa.ru',1901,112,'М','1123'),(10,'Николай','Валуев','Valued23@mail.ru',1971,400,'М','1333'),(11,'Себастьян','Куросава','Dima666@mail.ru',1993,290,'М','9342'),(12,'Николай','Ключников','bane@mail.ru',1996,186,'М','89333'),(13,'Дмитрий','Черепаха','fixit@mail.ru',1998,99,'М','8888'),(14,'Рихард','Гиот','giot@gmail.com',1999,448,'М','6666'),(15,'Имбирь','Иванов','zzz@mail.ru',1999,199,'М','1918'),(16,'Степан','Задорнов','ossl@mail.ru',1999,199,'М','1919'),(17,'Квокпок','Рыбный','nec@yandex.ru',1999,15,'М','1222'),(18,'Клюква','керосин','Domoi@mail.ru',1999,93,'М','9999'),(19,'Никита','Тугриков','ABS@mail.ru',1993,199,'М','99'),(20,'Ник','Перун','zz4@mail.ru',1999,130,'М','9393'),(21,'Кевин','Митник','mitnik@hotmail.com',2005,499,'М','1444'),(22,'Гвэн','Стейси','Gwen@hotmail.com',1993,93,'Ж','1919'),(23,'Грин','Вич','MSS@mail.ru',2001,210,'М','1234'),(24,'Роджер','Рэббит','setrogen@mail.ru',1999,199,'М','4044');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-12-24 20:54:16
+-- Dump completed on 2015-01-03 19:00:02
