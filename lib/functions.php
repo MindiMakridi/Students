3dc14c2ec822ee0bcc5e7723e0b047dfbf21dd5a
MindiMakridi <thehabibass@gmail.com>
2015-02-06 17:41:22 +0300

    nice

--- lib/functions.php
+++ lib/functions.php
@@ -22,15 +22,31 @@ function getUserCode(){
 	return false;
 }
 
+function generateToken() {
+	 $string = "abcdefghijklmnopqrstuvwxyz1234567890";
+        $length = mb_strlen($string);
+        $cypher = "";
+        for ($i = 0; $i <= 10; $i++) {
+            $cypher .= mb_substr($string, mt_rand(0, $length - 1), 1);
+        }
+        $salt1 = "BlackBrier";
+        $salt2 = "ThreadStone";
+        
+        
+         return md5($salt1 . $cypher . $salt2);
+}
+
 function updateStudentCookie($name, $email){
  setcookie("studentscookie[email]", $email, time()+(7*24*60*60*42), "/");
  setcookie("studentscookie[name]", $name, time()+(7*24*60*60*42), "/");
+
 }
 
 function logOut(){
 	setcookie('studentscookie[email]', "", time()-3600, '/');
     setcookie('studentscookie[name]', "", time()-3600, '/');
     setcookie('studentscookie[code]', "", time()-3600, '/');
+    setcookie('studentscookie[token]', "", time()-3600, '/');
 }
 
 
--- profile.php
+++ profile.php
@@ -3,6 +3,14 @@ require_once "/lib/DataMapper.php";
 require_once "/lib/functions.php";
 require_once "/lib/PDO.php";
 
+if(isset($_COOKIE['studentscookie']['token'])){
+
+$token = $_COOKIE['studentscookie']['token'];
+}
+else{
+    setcookie("studentscookie[token]", generateToken(), time()+60*60*3, "/");
+}
+
 $message = "";
 if (isset($_GET['msg'])) {
     $message = $_GET['msg'];
@@ -21,6 +29,8 @@ if (getUserCode()) {
     $email        = getUserMail();
     $code         = getUserCode();
     $title        = getUserID();
+    
+    
     if (!($profile = $mapper->fetchProfile($code))) {
         throw new Exception("Не удалось загрузить профиль");
     }
@@ -54,21 +64,30 @@ if (isset($_POST['submitted'])) {
 
 
 if (isset($_POST['submitted']) && $profile->checkErrors()) {
+  
+
     if (!getUserCode()) {
         $profile->generateCode();
         $mapper->addStudent($profile);
         $report = "Вы успешно зарегистрировались";
         setcookie("studentscookie[code]", $profile->showCode(), time() + (7 * 24 * 60 * 60 * 42), "/");
-    } else {
+        updateStudentCookie($profile->showName(), $profile->showEmail());
+    } elseif($_POST['token']==$token) {
         $report = "Изменения сохранены";
         $mapper->editProfile($profile);
+        updateStudentCookie($profile->showName(), $profile->showEmail());
     }
-    updateStudentCookie($profile->showName(), $profile->showEmail());
+    else {
+        $report = "Произошла ошибка";
+    }
+    
     header("Location: http://students.ru/profile.php?msg=$report");
     die;
 }
 
 
+$token = $_COOKIE['studentscookie']['token'];
 include "/templates/header.html";
 include "/templates/profile.html";
 include "/templates/footer.html";
+
